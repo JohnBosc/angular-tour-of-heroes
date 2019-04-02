e77c2659df7869dd9e5583480e8556782d1ae70d
Jean Mengue <jean@Dell>
2019-04-02 16:47:11 -0500

    Learned how to create services

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -9,17 +10,22 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
-
-  ngOnInit() {
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Mr. Nice', color: 'yellow'}, // hero
+    {id: 12, name: 'Narco', color: 'green'},
+    {id: 13, name: 'Bombasto', color: 'purple'},
+    {id: 14, name: 'Celeritas', color: 'brown'},
+    {id: 15, name: 'Magneta', color: 'grey'},
+    {id: 16, name: 'RubberMan', color: 'tale'},
+    {id: 17, name: 'Dynama', color: 'red'},
+    {id: 18, name: 'Dr IQ', color: 'blue'},
+    {id: 19, name: 'Magma', color: 'pink'},
+    {id: 20, name: 'Tornado', color: 'lightgrey'}
+];
